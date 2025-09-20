0c51315540138bd34af577b690ddcb560ba7828e
Chandu naidu <chandumaddula3@gmail.com>
2025-09-20 15:40:39 +0530

    Update scanner.js

--- static/js/scanner.js
+++ static/js/scanner.js
@@ -1,1 +1,19 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // Send scanned data to backend
+    fetch("/scan_attendance", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ qr_data: decodedText })
+    })
+    .then(res => res.json())
+    .then(data => {
+        const msg = document.getElementById("message");
+        msg.innerText = data.message;
+    });
+}
 
+// Initialize html5-qrcode scanner
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", { fps: 10, qrbox: 250 }
+);
+html5QrcodeScanner.render(onScanSuccess);
